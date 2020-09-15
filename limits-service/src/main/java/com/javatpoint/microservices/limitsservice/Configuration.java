9015ccccb90b77019f8598e53aae8a9f6b3a5a01
Sony@133786 <swap.mane16@gmail.com>
2020-09-15 16:07:13 +0530

    1st commit

--- limits-service/src/main/java/com/javatpoint/microservices/limitsservice/Configuration.java
+++ limits-service/src/main/java/com/javatpoint/microservices/limitsservice/Configuration.java
@@ -1,4 +1,4 @@
-//
+///////
 package com.javatpoint.microservices.limitsservice;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
